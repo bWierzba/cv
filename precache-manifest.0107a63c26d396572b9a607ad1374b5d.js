2290fe882bce348cdfd918923da41314aa4fb594
wierzba <wierzbabartlomiej@gmail.com>
2021-02-23 09:21:46 +0100

    Updates

---
+++ precache-manifest.0107a63c26d396572b9a607ad1374b5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0faf7f11dd1adf7de4402f248083162",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "b9a8f657aada0719bbcf",
+    "url": "/cv/static/css/main.1cdd2a04.chunk.css"
+  },
+  {
+    "revision": "49f812644da1f42ce631",
+    "url": "/cv/static/js/2.ed049f86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv/static/js/2.ed049f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a8f657aada0719bbcf",
+    "url": "/cv/static/js/main.53a63cdd.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  },
+  {
+    "revision": "6c1f65b12e8e40c7217fb378b381156a",
+    "url": "/cv/static/media/code.6c1f65b1.jpg"
+  },
+  {
+    "revision": "01902d5ee0472335849f5e1cf1498a68",
+    "url": "/cv/static/media/me.01902d5e.jpg"
+  }
+]);
